4a5cff86cc713c6d8e7d2862782695c891a014d9
Björn <a3c@web.de>
2022-07-01 07:30:02 +0200

    added functionality + html output

--- tools/pr_merge_state/script.js
+++ tools/pr_merge_state/script.js
@@ -1,49 +1,126 @@
+let body
 
-var pulls = []
-var usefull_data = []
-get_pulls()
-setTimeout(() => {
-    get_usefull_data()
-    console.log(usefull_data)
-}, 1000);
+let pulls = [];
+let requests_open = 0;
+let assembled_info = [];
 
-function get_pulls() {
-    var request = new XMLHttpRequest();
-    request.onload = async_pulls
-    request.open('get', 'https://api.github.com/repos/letsgamedev/Suffragium/pulls', true)
-    request.send()
-}
-function async_pulls() {
-    var responseObj = JSON.parse(this.responseText);
-    pulls = responseObj
-    for (i = 0; i < pulls.length; i++) {
-        var request_issue = new XMLHttpRequest();
-        request_issue.extraInfo = i
-        request_issue.onload = async_pull_issue;
-        request_issue.open('get', pulls[i].issue_url, true)
-        request_issue.send()
-        var request_commits = new XMLHttpRequest();
-        request_commits.extraInfo = i
-        request_commits.onload = async_pull_commits;
-        request_commits.open('get', pulls[i].commits_url, true)
-        request_commits.send()
-    }
-}
-function async_pull_issue() {
-    var responseObj = JSON.parse(this.responseText);
-    pulls[this.extraInfo].issue_data = responseObj
-}
-function async_pull_commits() {
-    var responseObj = JSON.parse(this.responseText);
-    pulls[this.extraInfo].commits_data = responseObj
+function init(){
+	body = document.getElementsByTagName('body')[0];
+	body.innerHTML += '<h1>Suffragium pull requst overview</h1>';
+	fetch_api('https://api.github.com/repos/letsgamedev/Suffragium/pulls', callback_api_pulls);
 }
 
-function get_usefull_data() {
-    for (i = 0; i < pulls.length; i++) {
-        usefull_data[i] = {}
-        usefull_data[i].title = pulls[i].title
-        usefull_data[i].votes = [pulls[i].issue_data.reactions["+1"], pulls[i].issue_data.reactions["-1"]]
-        usefull_data[i].last_commit = pulls[i].commits_data[pulls[i].commits_data.length - 1].commit.committer.date
-        usefull_data[i].url = pulls[i].url
-    }
+function fetch_api(url, callback, params){
+	let request = new XMLHttpRequest();
+	request.onload = callback;
+	request.params = params;
+	request.open('get', url, true);
+	request.send();
+	requests_open += 1;
+}
+
+function callback_api_pulls() {
+	pulls = JSON.parse(this.responseText);
+	for (i = 0; i < pulls.length; i++){
+		let pull = pulls[i];
+		fetch_api(pull.issue_url, callback_api_pull_issue, {index: i});
+		fetch_api(pull.commits_url, callback_api_pull_commits, {index: i});
+	}
+	callback_finished();
+}
+
+function callback_api_pull_issue(){
+	let pull_issue = JSON.parse(this.responseText);
+	let index = this.params.index;
+	pulls[index].issue_data = pull_issue;
+	callback_finished();
+}
+
+function callback_api_pull_commits(){
+	let pull_commits = JSON.parse(this.responseText);
+	let index = this.params.index;
+	pulls[index].commits_data = pull_commits;
+	callback_finished();
+}
+
+function callback_finished(){
+	requests_open -= 1;
+	if(requests_open === 0){
+		assemble_info();
+	}
+}
+
+function assemble_info(){
+	let info = [];
+	for(i = 0; i < pulls.length; i++){
+		let pull = pulls[i];
+		let reactions = pull.issue_data.reactions;
+		let commits_data = pull.commits_data;
+		info[i] = {};
+		info[i].title = pull.title;
+		info[i].votes = {'+1': reactions['+1'], '-1': reactions['-1']};
+		info[i].last_commit_time = commits_data[commits_data.length - 1].commit.committer.date;
+		info[i].url = pull.url;
+	}
+	console.log(info);
+	assembled_info = info;
+	display();
+}
+
+function display(){
+	for(i = 0; i < assembled_info.length; i++){
+		let pull = assembled_info[i];
+		let html = '';
+		html += '<div class="pr_box"><h3>' + pull.title + '</h3>';
+		// url is not to the pr on github, but the api …
+		//body.innerHTML += '<p>' + pull.url + '</p>';
+		let last_commit_unix_ms = Date.parse(pull.last_commit_time);
+		let now_unix_ms = Date.now();
+		let time_difference_ms = now_unix_ms - last_commit_unix_ms;
+		let last_commit_days = calculate_days(time_difference_ms);
+		html += '<p>last commit: ' + last_commit_days + ' days ago</p>';
+		let votes_up = pull.votes['+1'];
+		let votes_down = pull.votes['-1'];
+		let vote_count = votes_up + votes_down;
+		html += '<p>';
+		for(k = 0; k < votes_up; k++){
+			// thumb up/down is harder to see at a glance (color difference)
+			//body.innerHTML += '👍';
+			html += '🟩';
+		}
+		for(l = 0; l < votes_down; l++){
+			//body.innerHTML += '👎';
+			html += '🟥';
+		}
+		let percentage = 0;
+		if(vote_count > 0){
+			percentage = Math.round(votes_up / vote_count * 1000) / 10;
+		}
+		html += ' ' + percentage + '% (' + vote_count + ' votes)</p>';
+		html += '<div class="status_div"><span>status: </span>' + get_status(votes_up, votes_down, last_commit_days) + '</div></div><br>';
+		body.innerHTML += html;
+	}
+}
+
+function calculate_days(time_ms){
+	let seconds = time_ms / 1000;
+	let minutes = seconds / 60;
+	let hours = minutes / 60;
+	let days = Math.round(hours / 24 * 100) / 100;
+	return days;
+}
+
+function get_status(votes_up, votes_down, last_commit_days){
+	let vote_count = votes_up + votes_down;
+	let positive_votes = votes_up / vote_count;
+	if(last_commit_days >= 1 && vote_count > 10 && positive_votes >= 0.75){
+		return '<p class="status status_merge">MERGE</p>';
+	}
+	if(last_commit_days >= 3 && positive_votes >= 0.75){
+		return '<p class="status status_merge">MERGE</p>';
+	}
+	if(last_commit_days > 30){
+		return '<p class="status status_delete">DELETE</p>';
+	}
+	return '<p class="status status_active">active</p>';
 }
